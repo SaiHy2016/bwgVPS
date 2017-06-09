a5fc3fec517714a5e547032b79eb0df838d3fd8c
SaiHy2016 <huyuan2016@yeah.net>
2017-06-09 17:28:44 +0800

    字体图标

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'hy' });
-  res.render('index', { title: 'wellcome', lists: ['博客','关于我'] })
+  res.render('index', { title: 'wellcome', lists: [{ name: '博客', url:'https://saihy2016.github.io/' }, { name: '关于我',url:'' }, { name: '关于她',url:'' }] })
 });
 
 module.exports = router;
