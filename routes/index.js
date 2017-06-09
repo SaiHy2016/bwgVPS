dd903da4eeca0f2f63024efb9efc3b65e0b16e03
SaiHy2016 <huyuan2016@yeah.net>
2017-06-09 13:43:07 +0800

    玩玩ejs模板

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'hy' });
-  res.render('index.html');
+  res.render('index', { title: 'wellcome', lists: ['博客','关于我'] })
 });
 
 module.exports = router;
